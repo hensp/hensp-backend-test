bc85f421ec9f2658924951208285193d95c7382f
Gerson Contreras <gacvillalta33@gmail.com>
2023-04-25 14:41:42 -0600

    Gerson Contreras - Prueba backend-test

--- prueba-gerson-contreras/index.js
+++ prueba-gerson-contreras/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res)=>{
 res.status = 200;
-res.setHeader('Content-Type', 'text/plain');
+res.setHeader('Content-Type', 'text/plai; charset=utf-8');
 res.end('¡Bienvenido/a!');
 });
 server.listen(80, () => {
@@ -10,10 +10,27 @@ server.listen(80, () => {
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-
 const app = express();
 const port = 3306;
-const conexion = require("./select");
-const conexion2 = require("./create");
-const conexion3 = require("./update");
-const conexion4 = require("./delete");
+// Cargar otros .js
+const seleccionar = require("./select");
+const agregar = require("./create");
+const editar = require("./update");
+const eliminar = require("./delete");
+/*/ Modelo de usuario
+const connection = mysql.createConnection({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+/*/
